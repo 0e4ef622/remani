0304d0b4551e4b2ac7446a0e82565867aef6b39c
Cilan <abraham.hamidi01@gmail.com>
2018-02-01 23:49:18 -0600

    It compiles now

---
+++ src/skin/mod.rs
@@ -0,0 +1,88 @@
+//! A module for reading skins.
+
+extern crate graphics;
+
+use graphics::image::Image;
+
+use std::io;
+use std::error;
+use std::convert;
+use std::fmt;
+use std::path;
+
+mod osu_parser;
+
+use self::osu_parser::OsuParser;
+
+/// The error type from parsing
+#[derive(Debug)]
+pub enum ParseError {
+    /// IO error
+    Io(String, io::Error),
+    /// Parsing error
+    Parse(String, Option<Box<error::Error>>),
+    UnknownFormat,
+    InvalidFile,
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ParseError::Io(ref s, ref e) => write!(f, "{}: {}", s, e),
+            ParseError::Parse(ref s, Some(ref e)) => write!(f, "{}: {}", s, e),
+            ParseError::Parse(ref s, None) => write!(f, "{}", s),
+            ParseError::UnknownFormat => write!(f, "Unknown skin format"),
+            ParseError::InvalidFile => write!(f, "Invalid skin"),
+        }
+    }
+}
+
+impl From<io::Error> for ParseError {
+    fn from(error: io::Error) -> Self {
+        ParseError::Io(String::new(), error)
+    }
+}
+
+impl error::Error for ParseError {
+    fn description(&self) -> &str {
+        match *self {
+            ParseError::Io(_, _) => "IO error",
+            ParseError::Parse(_, _) => "Parse error",
+            ParseError::UnknownFormat => "Unknown skin format",
+            ParseError::InvalidFile => "Invalid skin",
+        }
+    }
+    fn cause(&self) -> Option<&error::Error> {
+        use std::ops::Deref;
+        match *self {
+            ParseError::Io(_, ref e) => Some(e),
+            ParseError::Parse(_, Some(ref e)) => Some(e.deref()),
+            _ => Some(self),
+        }
+    }
+}
+
+/// Holds skin data, such as note images and what not.
+#[derive(Default)]
+pub struct Skin {
+    pub mania_hit: Vec<Image>,
+}
+
+impl Skin {
+
+    /// Parse from a directory specified by the path.
+    ///
+    /// For now, the osu parser is assumed.
+    pub fn from_path<P: AsRef<path::Path>>(path: P) -> Result<Skin, ParseError> {
+
+        let parser = OsuParser::new(path::PathBuf::new().join(&path));
+        parser.parse()
+    }
+}
+
+/// A skin parser. Should be implemented by skin builders/parsers.
+trait SkinParser {
+
+    /// Parse the directory
+    fn parse(self) -> Result<Skin, ParseError>;
+}
--- src/skin/osu_parser.rs
+++ src/skin/osu_parser.rs
@@ -3,32 +3,33 @@
 use std::io;
 use std::io::BufRead;
 use std::path;
+use std::fs;
 
 use skin::{ Skin, SkinParser, ParseError };
 
 /// Loads osu skin images from directory and returns a `Skin`
-pub struct OsuParser<P: io::path::Path> {
-    dir: io::path::Path<P>,
+pub struct OsuParser {
+    dir: path::PathBuf,
 }
 
-impl<P: path::Path> OsuParser<P> {
+impl OsuParser {
 
     /// Create a new parser
-    pub fn new(dir: P) -> Self {
+    pub fn new(dir: path::PathBuf) -> Self {
         Self {
-            dir: P,
+            dir: dir,
         }
     }
 }
 
-impl<P: path::Path> SkinParser for OsuParser<P> {
+impl SkinParser for OsuParser {
 
     fn parse(self) -> Result<Skin, ParseError> {
         // TODO: read configuration file
-        let configPath = dir.join(Path::new("config.ini"));
-        let skin = Skin::default;
-        if (configPath.exists()) {
-            let reader = BufReader::new(File::open(configPath));
+        let configPath = self.dir.join(path::Path::new("skin.ini"));
+        let skin = Skin::default();
+        if configPath.exists() {
+            let reader = io::BufReader::new(fs::File::open(configPath)?);
             for line in reader.lines() {
                 let line = line?;
                 let line = line.trim();
