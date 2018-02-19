24b16bee7dd875b9df2454692b493bedad48ec3f
Matthew Tran <0e4ef622@gmail.com>
2018-02-18 18:51:08 -0600

    Parse hit objects in osu parser

--- src/chart/osu_parser.rs
+++ src/chart/osu_parser.rs
@@ -83,7 +83,7 @@ fn parse_timing_points(line: &str, chart: &mut IncompleteChart, last_bpm_change_
 
     let mut absolute = true;
 
-    for (index, field) in line.split(',').enumerate().take(7) {
+    for (index, field) in line.split(',').enumerate().take(8) {
 
         // Keep track of how many fields there were
         last_index = index;
@@ -123,6 +123,189 @@ fn parse_timing_points(line: &str, chart: &mut IncompleteChart, last_bpm_change_
 
         Ok(last_bpm_change_index.unwrap())
     }
+}
+
+
+/// Parse a line from the HitObjects section
+fn parse_hit_object(line: &str, chart: &mut IncompleteChart) -> Result<(), ParseError> {
+
+    let mut last_index = 0;
+    const ERR_STRING: &'static str = "Error parsing hit object";
+
+    let mut hit_obj = HitObject::default();
+    for (index, field) in line.split(',').enumerate().take(6) {
+
+        // Keep track of how many fields there were
+        last_index = index;
+
+        let mut ln = false;
+
+        match index {
+            0 => {
+                // calculate column
+
+                let n = cvt_err!(ERR_STRING, field.parse::<f64>())?;
+                const cw: f64 = 512.0 / 7.0;
+                let mut c = (n / cw).floor();
+                if c < 0.0 { c = 0.0; }
+                else if c > 7.0 { c = 7.0; }
+                hit_obj.column = c as u8;
+            }
+            1 => (),
+            2 => hit_obj.time = cvt_err!(ERR_STRING, field.parse::<f64>())? / 1000.0,
+            3 => ln = cvt_err!(ERR_STRING, field.parse::<u8>())? & 0xE == 0xE,
+            4 => {
+                let n = cvt_err!(ERR_STRING, field.parse::<u8>())?;
+
+                // constructs a hitsound with some default values
+                macro_rules! dflt_hit_snd {
+                    ($e:expr) => {
+                        HitSound {
+                            volume: 100,
+                            source: HitSoundSource::SampleSet(SampleHitSound {
+                                set: SampleSet::Auto,
+                                sound: $e,
+                                custom_index: 0,
+                            }),
+                        }
+                    }
+                }
+                if n & 2 == 2 { hit_obj.sounds.push(dflt_hit_snd!(SampleHitSoundSound::Whistle)); }
+                if n & 4 == 4 { hit_obj.sounds.push(dflt_hit_snd!(SampleHitSoundSound::Finish)); }
+                if n & 8 == 8 { hit_obj.sounds.push(dflt_hit_snd!(SampleHitSoundSound::Clap)); }
+            },
+            5 => {
+                let mut extras = field.split(':');
+                if ln {
+                    hit_obj.end_time = Some(match extras.next() {
+                        Some(s) => cvt_err!(ERR_STRING, s.parse::<f64>())? / 1000.0,
+                        None => return Err(ParseError::Parse(ERR_STRING.to_owned(), Some(Box::new(ParseError::EOL)))),
+                    });
+                }
+                for (i, v) in extras.enumerate().take(5) {
+                    let mut volume = 100;
+                    match i {
+                        0 => {
+                            match cvt_err!(ERR_STRING, v.parse::<u8>())? {
+                                0 => (),
+                                1 => hit_obj.sounds.iter_mut().for_each(|s| if let HitSoundSource::SampleSet(ref mut shs) = s.source { shs.set = SampleSet::Normal }),
+                                2 => hit_obj.sounds.iter_mut().for_each(|s| if let HitSoundSource::SampleSet(ref mut shs) = s.source { shs.set = SampleSet::Soft }),
+                                3 => hit_obj.sounds.iter_mut().for_each(|s| if let HitSoundSource::SampleSet(ref mut shs) = s.source { shs.set = SampleSet::Drum }),
+                                _ => (),
+                            }
+                        },
+                        1 => (),
+                        2 => {
+                            match cvt_err!(ERR_STRING, v.parse::<u32>())? {
+                                0 => (),
+                                n => {
+                                    hit_obj.sounds.iter_mut().for_each(|s| if let HitSoundSource::SampleSet(ref mut shs) = s.source { shs.custom_index = n });
+                                }
+                            }
+                        },
+                        3 => {
+                            let n = cvt_err!(ERR_STRING, v.parse::<u8>())?;
+                            if n != 0 {
+                                hit_obj.sounds.iter_mut().for_each(|s| s.volume = n);
+                                volume = n;
+                            }
+                        },
+                        4 => {
+                            hit_obj.sounds = vec![ HitSound {
+                                volume: volume,
+                                source: HitSoundSource::File(PathBuf::from(v)),
+                            } ];
+                        },
+                        _ => unreachable!(),
+                    }
+                }
+            }
+            _ => unreachable!()
+        }
+    }
+    chart.hit_objects.push(hit_obj);
+    Ok(())
+}
+
+/// Represents a hit object. This will get converted into a Note once the file is parsed and we can
+/// get the audio samples for the hit sound.
+#[derive(Default,Debug)]
+struct HitObject {
+
+    /// Where the note begins, in seconds.
+    time: f64,
+    column: u8,
+    end_time: Option<f64>,
+    sounds: Vec<HitSound>,
+}
+
+impl HitObject {
+
+    //fn to_note(self, sound: Rc<something>) {
+    fn to_note(self) -> Note {
+        Note {
+            time: self.time,
+            column: self.column,
+            end_time: self.end_time,
+            //sound: mixed?_hitsounds,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct HitSound {
+
+    /// The audio source of the sample.
+    source: HitSoundSource,
+
+    /// The volume of the hit sound, from 0 to 100
+    volume: u8,
+}
+
+/// Where to get the audio source of the hit sound
+#[derive(Debug)]
+enum HitSoundSource {
+    SampleSet(SampleHitSound),
+    File(PathBuf),
+}
+
+/// A hit sound that comes from a sample set
+#[derive(Debug)]
+struct SampleHitSound {
+    set: SampleSet,
+    sound: SampleHitSoundSound,
+    custom_index: u32,
+}
+
+/// A sample set
+#[derive(Debug)]
+enum SampleSet {
+    Auto,
+    Normal,
+    Soft,
+    Drum,
+}
+
+#[derive(Debug)]
+enum SampleHitSoundSound {
+    Normal,
+    Whistle,
+    Finish,
+    Clap,
+}
+
+/// Used during parsing, gets finalized into a Chart once all the values are obtained.
+#[derive(Default)]
+struct IncompleteChart {
+    hit_objects: Vec<HitObject>,
+    timing_points: Vec<TimingPoint>,
+    creator: Option<String>,
+    artist: Option<String>,
+    artist_unicode: Option<String>,
+    song_name: Option<String>,
+    song_name_unicode: Option<String>,
+    difficulty_name: Option<String>,
+    music_path: Option<PathBuf>,
 }
 
 /// Parses .osu charts and returns a `Chart`
@@ -185,7 +368,8 @@ impl ChartParser for OsuParser {
                     ParseError::Parse(String::from("Could not find audio file"), None)),
             },
 
-            notes: self.chart.notes,
+            //notes: self.chart.notes,
+            notes: Default::default(),
             timing_points: self.chart.timing_points,
             creator: self.chart.creator,
             artist: self.chart.artist,
@@ -195,18 +379,4 @@ impl ChartParser for OsuParser {
             difficulty_name: self.chart.difficulty_name.unwrap_or(String::from("Unnamed")),
         })
     }
-}
-
-/// Used during parsing, gets finalized into a Chart once all the values are obtained.
-#[derive(Default)]
-struct IncompleteChart {
-    notes: Vec<Note>,
-    timing_points: Vec<TimingPoint>,
-    creator: Option<String>,
-    artist: Option<String>,
-    artist_unicode: Option<String>,
-    song_name: Option<String>,
-    song_name_unicode: Option<String>,
-    difficulty_name: Option<String>,
-    music_path: Option<PathBuf>,
 }
