ee010dc3e815074387b29525b6760c71ae420d78
Matthew Tran <0e4ef622@gmail.com>
2018-03-12 09:42:50 -0500

    Remove Default trait from OsuSkin

--- src/skin/osu_skin.rs
+++ src/skin/osu_skin.rs
@@ -20,7 +20,6 @@ use std::rc::Rc;
 use skin::{ Skin, ParseError };
 
 /// Holds skin data, such as note images and what not.
-#[derive(Default)]
 struct OsuSkin {
     miss: Vec<Texture>,
     hit50: Vec<Texture>,
@@ -29,11 +28,11 @@ struct OsuSkin {
     hit300g: Vec<Texture>,
 
     /// The images virtual keys under the judgement line.
-    keys: [Vec<Rc<Texture>>; 7],
+    keys: [Rc<Texture>; 7],
 
     /// The images of the virtual keys under the judgement line when the
     /// corresponding key on the keyboard is pressed.
-    keys_d: [Vec<Rc<Texture>>; 7],
+    keys_d: [Rc<Texture>; 7],
 
     /// The notes' images.
     notes: [Vec<Rc<Texture>>; 7],
@@ -142,82 +141,81 @@ fn texture_from_path<T: AsRef<path::Path>>(path: T, texture_settings: &TextureSe
 
 pub fn from_path(dir: &path::Path) -> Result<Box<Skin>, ParseError> {
     let config_path = dir.join(path::Path::new("skin.ini"));
-    let mut skin = Box::new(OsuSkin::default());
 
     let texture_settings = TextureSettings::new();
 
     // test
-    skin.miss.push(texture_from_path(dir.join("mania-hit0.png"), &texture_settings));
-    skin.hit50.push(texture_from_path(dir.join("mania-hit50.png"), &texture_settings));
-    skin.hit100.push(texture_from_path(dir.join("mania-hit100.png"), &texture_settings));
-    skin.hit300.push(texture_from_path(dir.join("mania-hit300.png"), &texture_settings));
-    skin.hit300g.push(texture_from_path(dir.join("mania-hit300g-0.png"), &texture_settings));
+    let miss = vec![texture_from_path(dir.join("mania-hit0.png"), &texture_settings)];
+    let hit50 = vec![texture_from_path(dir.join("mania-hit50.png"), &texture_settings)];
+    let hit100 = vec![texture_from_path(dir.join("mania-hit100.png"), &texture_settings)];
+    let hit300 = vec![texture_from_path(dir.join("mania-hit300.png"), &texture_settings)];
+    let hit300g = vec![texture_from_path(dir.join("mania-hit300g-0.png"), &texture_settings)];
 
     let key1 = Rc::new(texture_from_path(dir.join("mania-key1.png"), &texture_settings));
     let key2 = Rc::new(texture_from_path(dir.join("mania-key2.png"), &texture_settings));
     let key3 = Rc::new(texture_from_path(dir.join("mania-keyS.png"), &texture_settings));
-    skin.keys[0].push(key1.clone());
-    skin.keys[1].push(key2.clone());
-    skin.keys[2].push(key1.clone());
-    skin.keys[3].push(key3.clone());
-    skin.keys[4].push(key1.clone());
-    skin.keys[5].push(key2.clone());
-    skin.keys[6].push(key1.clone());
+    let keys = [key1.clone(),
+                key2.clone(),
+                key1.clone(),
+                key3.clone(),
+                key1.clone(),
+                key2.clone(),
+                key1.clone()];
 
     let key1_d = Rc::new(texture_from_path(dir.join("mania-key1D.png"), &texture_settings));
     let key2_d = Rc::new(texture_from_path(dir.join("mania-key2D.png"), &texture_settings));
     let key3_d = Rc::new(texture_from_path(dir.join("mania-keySD.png"), &texture_settings));
-    skin.keys_d[0].push(key1_d.clone());
-    skin.keys_d[1].push(key2_d.clone());
-    skin.keys_d[2].push(key1_d.clone());
-    skin.keys_d[3].push(key3_d.clone());
-    skin.keys_d[4].push(key1_d.clone());
-    skin.keys_d[5].push(key2_d.clone());
-    skin.keys_d[6].push(key1_d.clone());
+    let keys_d = [key1_d.clone(),
+                  key2_d.clone(),
+                  key1_d.clone(),
+                  key3_d.clone(),
+                  key1_d.clone(),
+                  key2_d.clone(),
+                  key1_d.clone()];
 
     let note1 = Rc::new(texture_from_path(dir.join("mania-note1.png"), &texture_settings));
     let note2 = Rc::new(texture_from_path(dir.join("mania-note2.png"), &texture_settings));
     let note3 = Rc::new(texture_from_path(dir.join("mania-noteS.png"), &texture_settings));
-    skin.notes[0].push(note1.clone());
-    skin.notes[1].push(note2.clone());
-    skin.notes[2].push(note1.clone());
-    skin.notes[3].push(note3.clone());
-    skin.notes[4].push(note1.clone());
-    skin.notes[5].push(note2.clone());
-    skin.notes[6].push(note1.clone());
+    let notes = [vec![note1.clone()],
+                 vec![note2.clone()],
+                 vec![note1.clone()],
+                 vec![note3.clone()],
+                 vec![note1.clone()],
+                 vec![note2.clone()],
+                 vec![note1.clone()]];
 
     let ln1_head = Rc::new(texture_from_path(dir.join("mania-note1H.png"), &texture_settings));
     let ln2_head = Rc::new(texture_from_path(dir.join("mania-note2H.png"), &texture_settings));
     let ln3_head = Rc::new(texture_from_path(dir.join("mania-noteSH.png"), &texture_settings));
-    skin.long_notes_head[0].push(ln1_head.clone());
-    skin.long_notes_head[1].push(ln2_head.clone());
-    skin.long_notes_head[2].push(ln1_head.clone());
-    skin.long_notes_head[3].push(ln3_head.clone());
-    skin.long_notes_head[4].push(ln1_head.clone());
-    skin.long_notes_head[5].push(ln2_head.clone());
-    skin.long_notes_head[6].push(ln1_head.clone());
+    let long_notes_head = [vec![ln1_head.clone()],
+                           vec![ln2_head.clone()],
+                           vec![ln1_head.clone()],
+                           vec![ln3_head.clone()],
+                           vec![ln1_head.clone()],
+                           vec![ln2_head.clone()],
+                           vec![ln1_head.clone()]];
 
     let ln1_body = Rc::new(texture_from_path(dir.join("mania-note1L-0.png"), &texture_settings));
     let ln2_body = Rc::new(texture_from_path(dir.join("mania-note2L-0.png"), &texture_settings));
     let ln3_body = Rc::new(texture_from_path(dir.join("mania-noteSL-0.png"), &texture_settings));
-    skin.long_notes_body[0].push(ln1_body.clone());
-    skin.long_notes_body[1].push(ln2_body.clone());
-    skin.long_notes_body[2].push(ln1_body.clone());
-    skin.long_notes_body[3].push(ln3_body.clone());
-    skin.long_notes_body[4].push(ln1_body.clone());
-    skin.long_notes_body[5].push(ln2_body.clone());
-    skin.long_notes_body[6].push(ln1_body.clone());
+    let long_notes_body = [vec![ln1_body.clone()],
+                           vec![ln2_body.clone()],
+                           vec![ln1_body.clone()],
+                           vec![ln3_body.clone()],
+                           vec![ln1_body.clone()],
+                           vec![ln2_body.clone()],
+                           vec![ln1_body.clone()]];
 
-    skin.stage_hint = Some(Rc::new(texture_from_path(dir.join("mania-stage-hint.png"), &texture_settings)));
-    skin.stage_left = Some(Rc::new(texture_from_path(dir.join("mania-stage-left.png"), &texture_settings)));
-    skin.stage_right = Some(Rc::new(texture_from_path(dir.join("mania-stage-right.png"), &texture_settings)));
+    let stage_hint = Some(Rc::new(texture_from_path(dir.join("mania-stage-hint.png"), &texture_settings)));
+    let stage_left = Some(Rc::new(texture_from_path(dir.join("mania-stage-left.png"), &texture_settings)));
+    let stage_right = Some(Rc::new(texture_from_path(dir.join("mania-stage-right.png"), &texture_settings)));
     // end test
 
     // default values
-    skin.column_start = 136;
-    skin.column_width = vec!(30, 30, 30, 30, 30, 30, 30);
-    skin.column_line_width = vec!(2, 2, 2, 2, 2, 2, 2);
-    skin.hit_position = 402;
+    let mut column_start = 136;
+    let mut column_width = vec!(30, 30, 30, 30, 30, 30, 30);
+    let mut column_line_width = vec!(2, 2, 2, 2, 2, 2, 2, 2);
+    let mut hit_position = 402;
 
     if config_path.exists() {
         let config_file = File::open(config_path).unwrap();
@@ -242,20 +240,18 @@ pub fn from_path(dir: &path::Path) -> Result<Box<Skin>, ParseError> {
                 _ => {
                     if keys == 7 {
                         match key {
-                            "ColumnStart" => skin.column_start = value.parse().unwrap(),
-                            "HitPosition" => skin.hit_position = value.parse().unwrap(),
+                            "ColumnStart" => column_start = value.parse().unwrap(),
+                            "HitPosition" => hit_position = value.parse().unwrap(),
                             "ColumnWidth" => {
-                                skin.column_width = Vec::with_capacity(7);
                                 let number_strings: Vec<&str> = value.split(",").collect();
-                                for number_string in number_strings {
-                                    skin.column_width.push(number_string.parse().unwrap());
+                                for (i, number_string) in number_strings.iter().enumerate() {
+                                    column_width[i] = number_string.parse().unwrap();
                                 }
                             },
                             "ColumnLineWidth" => {
-                                skin.column_line_width = Vec::with_capacity(8);
                                 let number_strings: Vec<&str> = value.split(",").collect();
-                                for number_string in number_strings {
-                                    skin.column_line_width.push(number_string.parse().unwrap());
+                                for (i, number_string) in number_strings.iter().enumerate() {
+                                    column_line_width[i] = number_string.parse().unwrap();
                                 }
                             },
                             _ => (),
@@ -268,10 +264,29 @@ pub fn from_path(dir: &path::Path) -> Result<Box<Skin>, ParseError> {
     let smallest_note_width;
     let smallest_note_height;
     {
-        let smallest_height_note = &skin.notes.iter().min_by_key(|x| x[0].get_height()).unwrap()[0];
+        let smallest_height_note = &notes.iter().min_by_key(|x| x[0].get_height()).unwrap()[0];
         smallest_note_width = smallest_height_note.get_width() as f64;
         smallest_note_height = smallest_height_note.get_height() as f64;
     }
-    skin.width_for_note_height_scale =  smallest_note_height / smallest_note_width * *skin.column_width.iter().min().unwrap() as f64;
-    Ok(skin)
+    let width_for_note_height_scale =  smallest_note_height / smallest_note_width * *column_width.iter().min().unwrap() as f64;
+    Ok(Box::new(OsuSkin {
+        miss,
+        hit50,
+        hit100,
+        hit300,
+        hit300g,
+        keys,
+        keys_d,
+        notes,
+        long_notes_head,
+        long_notes_body,
+        stage_hint,
+        stage_left,
+        stage_right,
+        column_start,
+        column_width,
+        column_line_width,
+        hit_position,
+        width_for_note_height_scale,
+    }))
 }
