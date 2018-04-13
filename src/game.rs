69cdb26888b81dea5d14b955753bcb38f6a31e83
Matthew Tran <0e4ef622@gmail.com>
2018-04-12 22:30:08 -0500

    Add basic timing judgement

--- src/game.rs
+++ src/game.rs
@@ -22,7 +22,7 @@ pub fn start(config: Config) {
 
     let opengl = OpenGL::V3_2;
 
-    let mut window: Window = WindowSettings::new("Remani", [1024, 768])
+    let mut window: Window = WindowSettings::new("Remani", [1440, 900])
                              .opengl(opengl)
                              .srgb(false)
                              .build()
@@ -43,19 +43,25 @@ pub fn start(config: Config) {
 
     let the_skin = skin::from_path(&config.skin_path, &config).unwrap();
 
-    let mut model = Model::new();
+    let mut model = Model::new(&chart);
     let mut view = View::new(GlGraphics::new(opengl), the_skin, &chart);
 
     let mut events = Events::new(EventSettings::new());
     let mut time = -config.offset;
     let mut last_instant = time::Instant::now();
+    let mut first_playhead_received = false;
     audio.request_playhead();
     while let Some(e) = events.next(&mut window) {
 
         if let Some((instant, playhead)) = audio.get_playhead() {
 
             let d = instant.elapsed();
-            time = playhead + d.as_secs() as f64 + d.subsec_nanos() as f64 / 1_000_000_000.0 - config.offset;
+            let new_time = playhead + d.as_secs() as f64 + d.subsec_nanos() as f64 / 1_000_000_000.0 - config.offset;
+            if !first_playhead_received {
+                time = new_time;
+            } else {
+                time = (time + new_time) / 2.0;
+            }
             last_instant = time::Instant::now();
             audio.request_playhead();
 
@@ -68,15 +74,15 @@ pub fn start(config: Config) {
         }
 
         if let Some(u) = e.update_args() {
-            model.update(&u);
+            model.update(&u, time);
         }
 
         if let Some(i) = e.press_args() {
-            model.press(&i, &config);
+            model.press(&i, &config, time, |k| ());
         }
 
         if let Some(i) = e.release_args() {
-            model.release(&i, &config);
+            model.release(&i, &config, time, |k| ());
         }
 
         if let Some(r) = e.render_args() {
--- src/model.rs
+++ src/model.rs
@@ -1,40 +1,113 @@
 //! A module that handles window update events
 
+use std::collections::VecDeque;
+
 use piston::input::{ UpdateArgs, Button };
 
 use config::Config;
+use chart::Chart;
 
-/// Holds game states needed by the logic and renderer
-pub struct Model {
+/// Holds game states needed by the logic and renderer. Also does timing judgements.
+pub struct Model<'a> {
     pub keys_down: [bool; 7],
+    chart: &'a Chart,
+
+    /// Contains the index of the first note that is 1 second ahead of the current time.
+    current_note_index: usize,
+
+    /// Contains the indices of all the notes that are to be hit within the next second or haven't
+    /// been hit yet (1 second into the future, 1 second into the past), categorized into which
+    /// column they are on.
+    next_notes: [VecDeque<usize>; 7],
+
+    /// Whether the column is currently holding a long note, and if so, contains the index of the
+    /// note
+    long_notes_held: [Option<usize>; 7],
 }
 
-impl Model {
+impl<'a> Model<'a> {
 
     /// Create a model for the game controller
-    pub fn new() -> Self {
-        Self {
-            keys_down: [false; 7]
+    pub fn new(chart: &Chart) -> Model {
+        Model {
+            keys_down: [false; 7],
+            chart,
+            current_note_index: 0,
+            next_notes: [VecDeque::with_capacity(32),
+                         VecDeque::with_capacity(32),
+                         VecDeque::with_capacity(32),
+                         VecDeque::with_capacity(32),
+                         VecDeque::with_capacity(32),
+                         VecDeque::with_capacity(32),
+                         VecDeque::with_capacity(32)],
+            long_notes_held: [None; 7],
         }
     }
 
     /// Called when an update event occurs
-    pub fn update(&mut self, args: &UpdateArgs) {
-        // stuff
+    pub fn update(&mut self, args: &UpdateArgs, time: f64) {
+        // how many notes should be removed from the front of each vecdeque
+        let mut to_be_removed = [0; 7];
+
+        for (column, note_vec) in self.next_notes.iter().enumerate() {
+
+            for &note_index in note_vec {
+
+                let note = &self.chart.notes[note_index];
+                if let Some(end_time) = note.end_time {
+
+                    if end_time - time < -1.0 {
+                        println!("Miss");
+                        to_be_removed[column] += 1;
+                    }
+                } else if note.time - time < -1.0 {
+                    println!("Miss");
+                    to_be_removed[column] += 1;
+                }
+            }
+        }
+
+        for (column, &n) in to_be_removed.iter().enumerate() {
+            for _ in 0..n {
+                self.next_notes[column].pop_front();
+            }
+        }
+
+        while self.chart.notes[self.current_note_index].time - time < 1.0 {
+            self.next_notes[self.chart.notes[self.current_note_index].column].push_back(self.current_note_index);
+            self.current_note_index += 1;
+        }
     }
 
     /// Called when a press event occurs
-    pub fn press(&mut self, args: &Button, config: &Config) {
-        config.key_bindings.iter().zip(self.keys_down.iter_mut())
-            .for_each(|(key_binding, key_down)| {
-                if *args == *key_binding { *key_down = true; }
+    pub fn press<F: FnMut(usize)>(&mut self, args: &Button, config: &Config, time: f64, mut callback: F) {
+
+        let next_notes = &mut self.next_notes;
+        let chart = self.chart;
+
+        config.key_bindings.iter().enumerate().zip(self.keys_down.iter_mut())
+            .for_each(|((key_index, key_binding), key_down)| {
+                if *args == *key_binding && !*key_down {
+                    if let Some(&note_index) = next_notes[key_index].get(0) {
+                        let note = &chart.notes[note_index];
+                        if (note.time - time).abs() < 0.2 {
+                            println!("Nice");
+                        }
+                        next_notes[key_index].pop_front();
+                    }
+                    callback(key_index);
+                    *key_down = true;
+                }
             });
     }
 
-    pub fn release(&mut self, args: &Button, config: &Config) {
-        config.key_bindings.iter().zip(self.keys_down.iter_mut())
-            .for_each(|(key_binding, key_down)| {
-                if *args == *key_binding { *key_down = false; }
+    pub fn release<F: FnMut(usize)>(&mut self, args: &Button, config: &Config, time: f64, mut callback: F) {
+        config.key_bindings.iter().enumerate().zip(self.keys_down.iter_mut())
+            .for_each(|((key_index, key_binding), key_down)| {
+                if *args == *key_binding {
+                    callback(key_index);
+                    *key_down = false;
+                }
             });
     }
 
