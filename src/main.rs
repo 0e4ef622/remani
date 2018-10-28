cc7a1757bc3d36b9f853ff5cf00f96336d41207b
Matthew Tran <0e4ef622@gmail.com>
2018-10-27 21:56:58 -0500

    Rename default config file

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     let config_path: PathBuf = env::var_os("REMANI_CONF")
         .map(|s| s.into())
-        .unwrap_or(ProjectDirs::from("", "0e4ef622", "Remani").unwrap().config_dir().join("remani.conf"));
+        .unwrap_or(ProjectDirs::from("", "0e4ef622", "Remani").unwrap().config_dir().join("config.toml"));
 
     let config = config::get_config(&config_path);
     window::start(config);
